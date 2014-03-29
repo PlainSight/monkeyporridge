0d5ee8e8319ea242690e4e4d2df8ab495f7d1c88
Alex Ogier <alexander.ogier@gmail.com>
2014-03-29 16:28:40 +1300

    nodeserver js file
    password and database redacted

---
+++ nodeserver.js
@@ -0,0 +1,94 @@
+var http = require("http");
+var url = require("url");
+var mysql = require("mysql");
+var qs = require("querystring");
+
+var connection = mysql.createConnection({
+	user: "root",
+	password: "redacted",
+	database: "redacted",
+	host: "127.0.0.1",
+	port: "3306",
+	socketPath: "/var/run/mysqld/mysqld.sock"
+});
+
+function start(route) {
+
+	function onRequest(request, response) {		
+
+		var pathname = url.parse(request.url).pathname;
+		
+
+		if(pathname == '/addscore') {
+			var body = '';
+			request.on('data', function(data) {
+				body += data;
+				var postdata = qs.parse(body);
+
+				var confirmationhash = new String(postdata.name + postdata.score + 
+				postdata.outertick).hashCode();
+				
+				postdata.name = postdata.name.replace("'", "''");
+
+				if((postdata.score > 2000) && (typeof postdata.score !== 'number') && postdata.hash !== confirmationhash) {
+					replyWithScore(response);	
+				} else {
+
+				connection.query("insert into scores values('" + postdata.name.substring(0, 40) + 
+						"', " + postdata.score +
+			 ", now(),'" + request.connection.remoteAddress + "')", function(error, rows, fields) {
+					replyWithScore(response);
+				});
+
+				}
+			});
+			
+			return;
+		}
+
+		replyWithScore(response);			
+
+		
+	}
+	
+	http.createServer(onRequest).listen(8888);
+
+	console.log('server started');
+}
+
+function replyWithScore(response) {
+		connection.query('select * from scores order by score desc, date asc limit 20;', function(error, rows, fields) {
+
+                        response.writeHead(200, {
+                                        "Content-Type": "text/plain",
+                                        "access-control-allow-origin": "*",
+                                        "access-control-allow-methods": "GET, POST",
+                                        "access-control-allow-headers": "content-type, accept",
+                                        "access-control-max-age": 10 });
+                        response.end(JSON.stringify(rows));
+
+
+                });
+};
+
+
+String.prototype.hashCode = function(){
+	if (Array.prototype.reduce){
+		return this.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+	} 
+	var hash = 0;
+	if (this.length === 0) return hash;
+	for (var i = 0; i < this.length; i++) {
+		var character  = this.charCodeAt(i);
+		hash  = ((hash<<5)-hash)+character;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return hash;
+}
+
+function route(pathname) {
+
+}
+
+
+start(route);
